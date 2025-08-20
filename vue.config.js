98cb60af5805e13a025305e6c2762bda1a494b4a
Mikhail Mingareev <mingareev@myheat.net>
2025-08-20 17:10:07 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './', // Используйте относительные пути
+  outputDir: 'dist',
+  assetsDir: 'static',
+
   configureWebpack: {
     output: {
       publicPath: '/'

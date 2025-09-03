b4f01bcf2e98f5b69c074045e5894feca180a3ab
Mikhail Mingareev <mingareev@myheat.net>
2025-09-03 18:49:30 +0300

    fixes

--- src/components/AccountSettings.vue
+++ src/components/AccountSettings.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="settings-wrapper">
     <div class="page-header">
-      <div class="page-header-arrow"></div>
-      <div class="page-header-title"></div>
+      <div class="page-header-title">
+        <h5>Настроим частоту напоминаний</h5>
+      </div>
     </div>
-    <div class="photo-block"></div>
+    <div class="photo-block">
+      <img src="../assets/dog.png" alt="">
+    </div>
     <div class="day-block">
 <!--      <div class="circle-menu">-->
 <!--        <div class="circle-menu-content">-->
@@ -20,8 +23,8 @@
 <!--        </div>-->
 <!--      </div>-->
     </div>
-    <div style="display:flex;width: 100%;justify-content: center; align-items: flex-start; background: #00000020; backdrop-filter: blur(10px); padding: 30px; border-radius: 20px">
-      <div class="list">
+    <div class="list-wrapper">
+      <div class="list" style="margin-right: 20px">
         <div class="list-item">
           <p>Каждые две недели</p>
         </div>
@@ -38,6 +41,25 @@
           <p>Несколько раз в день</p>
         </div>
       </div>
+      <div class="list">
+        <div class="list-item">
+          <p>07:00</p>
+        </div>
+        <div class="list-item">
+          <p>09:00</p>
+        </div>
+        <div class="list-item">
+          <p>11:00</p>
+        </div>
+        <div class="list-item">
+          <p>13:00</p>
+        </div>
+        <div class="list-item">
+          <p>15:00</p>
+        </div>
+      </div>
+
+
 <!--      <div class="vert-checker">-->
 <!--        <div class="checker-wrapper">-->
 <!--          <div class="checker-wrapper-wheel">-->
@@ -64,8 +86,6 @@
 <!--        </div>-->
 <!--      </div>-->
     </div>
-    <button style="margin-top: 30px; padding: 10px" @click="turn()">Turn</button>
-    <button style="margin-top: 30px; padding: 10px" @click="ret()">Ret</button>
   </div>
 </template>
 <script lang="ts">
@@ -87,41 +107,95 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-.list {
-  .list-item {
-    margin: 5px 0;
+.page-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #000;
+  padding: 24px 10px;
+  box-shadow: 0 0 10px #ffffff20;
+  .page-header-arrow {
+    .back-button {
+      width: 50px;
+      background: none;
+      padding: 10px 20px;
+      border: none;
+      outline: none;
+      span {
+        color: #fff;
+      }
+    }
+  }
+  .page-header-title {
+    width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-    background: #00000050;
-    backdrop-filter: blur(10px);
-    &:nth-child(1) {
-      opacity: 0.09;
-    }
-    &:nth-child(2) {
-      opacity: 0.2;
-    }
-    &:nth-child(3) {
-      opacity: 1;
-      border: 1px solid #38E07A;
-      border-radius: 20px;
-      padding: 5px 20px;
-      p {
-        color: #38E07A
-      }
-    }
-    &:nth-child(4) {
-      opacity: 0.2;
-    }
-    &:nth-child(5) {
-      opacity: 0.09;
-    }
-    p {
+    h5 {
+      text-align: center;
       color: #fff;
-      font-size: 20px;
+      font-size: 18px;
     }
   }
 }
+
+.photo-block {
+  position: relative;
+  z-index: 100;
+  img {
+    width: 100%;
+  }
+}
+
+.list-wrapper {
+  display: flex;
+  width: 90%;
+  margin: auto;
+  justify-content: center;
+  align-items: flex-start;
+  background: #00000020;
+  backdrop-filter: blur(10px);
+  padding: 30px;
+  border-radius: 20px;
+  margin-top: -30px;
+  box-shadow: 0 0 10px #ffffff20;
+  .list {
+    .list-item {
+      margin: 5px 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #00000050;
+      backdrop-filter: blur(10px);
+      &:nth-child(1) {
+        opacity: 0.09;
+      }
+      &:nth-child(2) {
+        opacity: 0.2;
+      }
+      &:nth-child(3) {
+        opacity: 1;
+        border: 1px solid #38E07A;
+        border-radius: 20px;
+        padding: 5px 20px;
+        p {
+          color: #38E07A
+        }
+      }
+      &:nth-child(4) {
+        opacity: 0.2;
+      }
+      &:nth-child(5) {
+        opacity: 0.09;
+      }
+      p {
+        color: #fff;
+        font-size: 18px;
+      }
+    }
+  }
+}
+
 .vert-checker {
   width: 58px;
   height: 150px;
@@ -184,8 +258,6 @@ export default {
   margin-left: auto;
   margin-right: auto;
   max-width: 700px;
-  margin-top: 100px;
-  padding: 20px;
 }
 .circle-menu {
   margin: auto;

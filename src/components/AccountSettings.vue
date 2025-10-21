b5413f7109130549afb6773874f656720b12f85d
Mikhail Mingareev <mingareev@myheat.net>
2025-10-21 14:18:18 +0300

    add select

--- src/components/AccountSettings.vue
+++ src/components/AccountSettings.vue
@@ -30,6 +30,16 @@
 <!--      </div>-->
     </div>
 
+    <label for="start">Start date:</label>
+
+    <input
+        type="date"
+        id="start"
+        name="trip-start"
+        value="2018-07-22"
+        min="2018-01-01"
+        max="2018-12-31" />
+
     <!-- Начально будет выбрано второе значение -->
     <select name="select">
       <!--Supplement an id here instead of using 'name'-->
